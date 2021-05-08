d40cd0bfd4ac35aa86a9bb15945e6df0dc76316b
Lester Manuel <lester.manuel@mynt.xyz>
2021-05-08 11:01:48 +0800

    added voters registration fragments

---
+++ Voters/app/src/main/java/ph/apper/android/capstone/voters/RegistrationProcessingFragment.kt
@@ -0,0 +1,28 @@
+package ph.apper.android.capstone.voters
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class RegistrationProcessingFragment: Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_registration_processing, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.tv_close).setOnClickListener {
+            findNavController().navigate(R.id.action_RegistrationProcessingFragment_to_RegistrationPersonalFragment)
+        }
+    }
+}
