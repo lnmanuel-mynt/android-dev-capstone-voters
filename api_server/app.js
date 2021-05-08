920c0318f09b0fff8cb72cd1afe7cd6e5fae0d4d
jratienza <johnashly.atienza@mynt.xyz>
2021-05-08 20:38:06 +0800

    add voter registration

--- api_server/app.js
+++ api_server/app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const mysql = require("mysql");
 const bcrypt = require("bcrypt");
 const { v4: uuidv4, parse: uuidParse } = require("uuid");
+const e = require("express");
 const app = express();
 app.use(
     bodyParser.urlencoded({
@@ -24,13 +25,13 @@ const dbPool = mysql.createPool(db_config);
 app.post("/signup", async (req, res) => {
     var email = req.body.email;
     var password = await bcrypt.hash(req.body.password, 10);
-    var firstName = req.body.firstName;
-    var middleName = req.body.middleName;
-    var lastName = req.body.lastName;
+    var firstName = req.body.first_name;
+    var middleName = req.body.middle_name;
+    var lastName = req.body.last_name;
 
-    var isExisting = await checkIfExists(email);
+    var rows = getUserByEmail(email, "app_users");
 
-    if (isExisting) return res.status(401).send("Email already registered.");
+    if (rows != "") return res.status(400).send("Email already registered.");
 
     let insertQry = "INSERT INTO ??(??) VALUES (UUID_TO_BIN(?), ?)";
 
@@ -58,37 +59,134 @@ app.post("/login", async (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
 
-    selectQry = "SELECT * FROM ?? WHERE ?? LIKE ?";
+    var rows = await getUserByEmail(email, "app_users");
 
-    let query = mysql.format(selectQry, ["app_users", "email", email]);
-    try {
-        var results = await db_query(query, dbPool);
-        if (results == "")
+    if (rows == "")
+        return res.status(401).send({
+            message: "Invalid Credentials.",
+        });
+
+    if (rows[0].email != email)
+        return res.status(401).send({
+            message: "Invalid Credentials.",
+        });
+
+    bcrypt.compare(password, results[0].passwd, function (err, result) {
+        if (result == true)
             return res.status(200).send({
-                message: "Invalid Credentials.",
+                message: `${results[0].first_name} ${results[0].last_name} logged in.`,
+                profile: rows[0],
             });
-        bcrypt.compare(password, results[0].passwd, function (err, result) {
-            if (result == true)
-                return res.status(200).send({
-                    message: `${results[0].first_name} ${results[0].last_name} logged in.`,
-                });
-            return res.status(401).send({
-                message: "Invalid Credentials.",
-            });
+        return res.status(401).send({
+            message: "Invalid Credentials.",
+        });
+    });
+});
+
+app.post("/voter/register", async (req, res) => {
+    var firstName = req.body.first_name;
+    var middleName = req.body.middle_name;
+    var lastName = req.body.last_name;
+    var email = req.body.email;
+    var birthDate = req.body.birth_date;
+    var birthProvince = req.body.birth_province;
+    var birthCity = req.body.birth_city;
+    var civilStatus = req.body.civil_status;
+    var sex = req.body.sex;
+    var street = req.body.street;
+    var barangay = req.body.barangay;
+    var city = req.body.city;
+    var province = req.body.province;
+    var yearsInCity = req.body.years_in_city;
+    var yearsInPH = req.body.years_in_ph;
+    var appUserId = req.body.app_user_id;
+
+    var rows = await getUserById(appUserId, "voters");
+    if (rows != "")
+        return res.status(400).send({
+            message: "Voter already signed up.",
+        });
+
+    let insertQry = "INSERT INTO ??(??) VALUES (UUID_TO_BIN(?), ?)";
+
+    let query = mysql.format(insertQry, [
+        "voters",
+        [
+            "app_user_id",
+            "first_name",
+            "middle_name",
+            "last_name",
+            "email",
+            "birth_date",
+            "birth_province",
+            "birth_city",
+            "civil_status",
+            "sex",
+            "street",
+            "barangay",
+            "city",
+            "province",
+            "years_in_city",
+            "years_in_ph",
+            "isRegistered",
+            "voter_id_number",
+            "precinct_number",
+        ],
+        appUserId,
+        [
+            firstName,
+            middleName,
+            lastName,
+            email,
+            birthDate,
+            birthProvince,
+            birthCity,
+            civilStatus,
+            sex,
+            street,
+            barangay,
+            city,
+            province,
+            yearsInCity,
+            yearsInPH,
+            false,
+            null,
+            null,
+        ],
+    ]);
+
+    console.log(query);
+    try {
+        await db_query(query, dbPool);
+        res.status(201).send({
+            message: "Voter Application Successful.",
         });
     } catch (err) {
-        console.log(`Caught error while Logging in: ${err}`);
+        res.status(400).send({
+            message: `Voter Application Failed: ${err} `,
+        });
     }
 });
 
-async function checkIfExists(email) {
-    selectQry = "SELECT ?? FROM ?? WHERE ?? LIKE ?";
+async function getUserByEmail(email, table) {
+    var selectQry = "SELECT * FROM ?? WHERE ?? LIKE ?";
 
-    let query = mysql.format(selectQry, ["email", "app_users", "email", email]);
+    let query = mysql.format(selectQry, [table, "email", email]);
     try {
         var results = await db_query(query, dbPool);
-        if (results == "") return false;
-        return true;
+        return results;
+    } catch (err) {
+        console.log(`Caught Error while querying database: ${err}`);
+    }
+}
+
+async function getUserById(id, table) {
+    var selectQry = "SELECT * FROM ?? WHERE BIN_TO_UUID(??) LIKE ?";
+
+    let query = mysql.format(selectQry, [table, "app_user_id", id]);
+    try {
+        var results = await db_query(query, dbPool);
+        return results;
     } catch (err) {
         console.log(`Caught Error while querying database: ${err}`);
     }
