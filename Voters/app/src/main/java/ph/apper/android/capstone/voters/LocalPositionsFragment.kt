e18a7b9ac87d802d5237da2f295757d3d79ae3e6
jratienza <johnashly.atienza@mynt.xyz>
2021-05-12 22:15:11 +0800

    split local and national positions fragment

---
+++ Voters/app/src/main/java/ph/apper/android/capstone/voters/LocalPositionsFragment.kt
@@ -0,0 +1,63 @@
+package ph.apper.android.capstone.voters
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_local_positions.*
+import kotlinx.android.synthetic.main.fragment_local_positions.view.*
+import ph.apper.android.capstone.voters.adapters.CandidatePositionsAdapter
+
+
+class LocalPositionsFragment : Fragment(), View.OnClickListener{
+    lateinit var navController: NavController
+
+    companion object {
+        private lateinit var positionsAdapter: CandidatePositionsAdapter
+    }
+
+    private var localPositionsArray = arrayListOf(
+        "GOVERNOR",
+        "VICE-GOVERNOR",
+        "SANGGUNIANG PANLALAWIGAN",
+        "MAYOR",
+        "VICE-MAYOR",
+        "COUNCILOR",
+        "CONGRESSMAN"
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_local_positions, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+        btn_natl_positions.setOnClickListener(this)
+
+        view.rv_position_list.layoutManager = LinearLayoutManager(this.requireActivity().applicationContext)
+        LocalPositionsFragment.positionsAdapter = CandidatePositionsAdapter(localPositionsArray, this.requireActivity().applicationContext)
+        view.rv_position_list.adapter = LocalPositionsFragment.positionsAdapter
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            btn_natl_positions.id -> {
+                navController.navigate(R.id.action_localPositionsFragment_to_nationalPositionsFragment)
+            }
+        }
+    }
+}
