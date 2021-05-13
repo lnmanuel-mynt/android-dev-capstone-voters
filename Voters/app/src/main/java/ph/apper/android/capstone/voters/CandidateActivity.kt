c00dac82c4e49d2e38596fcaa2956006440c31fa
jratienza <johnashly.atienza@mynt.xyz>
2021-05-13 21:14:29 +0800

    use activity for fragment communication

--- Voters/app/src/main/java/ph/apper/android/capstone/voters/CandidateActivity.kt
+++ Voters/app/src/main/java/ph/apper/android/capstone/voters/CandidateActivity.kt
@@ -1,6 +1,28 @@
 package ph.apper.android.capstone.voters
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ph.apper.android.capstone.voters.model.CandidateInfo
 
 class CandidateActivity : AppCompatActivity(){
+    companion object{
+        var candidateList: ArrayList<CandidateInfo> = ArrayList()
+
+        var candidatePosition: String = ""
+
+        fun clearArray(){
+            candidateList.clear()
+        }
+
+        fun populateList(items: ArrayList<CandidateInfo>?){
+            candidateList = items!!
+        }
+
+        var selectedCandidate: CandidateInfo = CandidateInfo()
+
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_candidate)
+    }
 }
